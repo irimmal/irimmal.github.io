bee745c8a968f96f5d11dbb9e77868ba3601e6ee
irimmal <115705619+irimmal@users.noreply.github.com>
2023-05-11 18:32:28 +0800

    delayloading.js

--- delayloading.js
+++ delayloading.js
@@ -2,5 +2,5 @@ let interval = setInterval(()=>{
   if (document.querySelector(".footer")){
     document.querySelector(#customize").style.display="";
     clearInterval(interval);
-  )
-  },200)
+  }
+ },200);
